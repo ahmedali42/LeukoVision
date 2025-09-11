386ec07f4010adf240bb003b9c285ca7e2d9a238
ahmedali42 <ahmedali.4290@gmail.com>
2025-09-11 17:12:43 +0200

    update

--- Streamlit/pages/models.py
+++ Streamlit/pages/models.py
@@ -50,14 +50,12 @@ if section == "InceptionV3":
     st.markdown(""" <div style="text-align: justify;">
     The training accuracy approaches 99.97%, while the validation accuracy reaches 
     98.29%, demonstrating the model’s strong ability to accurately classify different cell types.
-                
                 </div>
                 """,unsafe_allow_html=True)
     if st.toggle("Show InceptionV3 loss and accuracy plot"):
         st.image(white_bg('./Streamlit/pages/images/inception/loss_acc.png'), caption='Loss and accruacy plot from InceptionV3 training',use_container_width=True)
     st.markdown(""" <div style="text-align: justify;">
                 The test set shows a very high accuracy of 98.28% and that is reflected in the digonal form of the confusion matrix.
-                
                 </div>
                 """,unsafe_allow_html=True)
     if st.toggle("Show InceptionV3 confusion matrix"):
