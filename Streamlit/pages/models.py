84d1feaee29156514f90056baec12c5e0e5b15ac
ahmedali42 <ahmedali.4290@gmail.com>
2025-09-11 16:42:22 +0200

    update

--- Streamlit/pages/models.py
+++ Streamlit/pages/models.py
@@ -4,6 +4,10 @@ import sys
 from utils import white_bg
 import io
 from tensorflow.keras.applications import InceptionV3
+import numpy as np
+from sklearn.metrics import classification_report
+import pandas as pd
+
 
 st.title('Modeling')
 
@@ -43,12 +47,20 @@ if section == "InceptionV3":
         model.summary(print_fn=lambda x: stream.write(x + "\n"))
         st.code(stream.getvalue())
     st.markdown('### Performace')
-    st.markdown("The training accuracy approaches 99.87%, while the validation accuracy reaches" \
-        " 98.75%, demonstrating the model’s strong ability to accurately classify different cell types.")
+    st.markdown("The training accuracy approaches 99.97%, while the validation accuracy reaches" \
+        " 98.29%, demonstrating the model’s strong ability to accurately classify different cell types.")
     if st.toggle("Show InceptionV3 loss and accuracy plot"):
         st.image(white_bg('./Streamlit/pages/images/inception/loss_acc.png'), caption='Loss and accruacy plot from InceptionV3 training',use_container_width=True)
+    st.markdown('The test set shows a very high accuracy of 98.28% and that is reflected in the digonal form of the confusion matrix.')
     if st.toggle("Show InceptionV3 confusion matrix"):
         st.image(white_bg('./Streamlit/pages/images/inception/cm.png'), caption='Confusion matrix of InceptionV3 test set',use_container_width=True)
+    y=np.loadtxt('./Streamlit/pages/images/inception/y_true_y_pred.txt',dtype=int)
+    y_true=y[:,0];y_pred=y[:,1]
+    report = classification_report(y_true, y_pred, target_names=['BAS','EOS','EBO','IG','LYT','MON','NGS','PLA'], output_dict=True)
+    df_report = pd.DataFrame(report).transpose()
+    st.dataframe(df_report.style.format(precision=2).set_table_styles(
+    [{'selector': 'th', 'props': [('text-align', 'center')]},
+     {'selector': 'td', 'props': [('text-align', 'center')]}]))
 
 # The key idea behind InceptionV3 is the use of **Inception modules**, which allow the network to capture features at multiple scales simultaneously. Each module applies several convolutions of different sizes in parallel and concatenates the results, enabling the model to learn both fine and coarse features from an image.  
 
