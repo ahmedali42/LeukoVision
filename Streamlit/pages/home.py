70d1b1243a325f7446e84f44317e820bd123d765
ahmedali42 <ahmedali.4290@gmail.com>
2025-09-12 10:01:10 +0200

    update

--- Streamlit/pages/home.py
+++ Streamlit/pages/home.py
@@ -4,19 +4,19 @@ st.set_page_config( layout="wide")
 
 st.title("🏠 Welcome to LeukoVision 🩸")
 
+with st.container():
+    st.markdown("""
+    <div style="text-align: justify;">
+                
+    **LeukoVision** is an advanced AI-powered platform designed to analyze and classify blood cell images with precision.  
+    Leveraging state-of-the-art deep learning models — **InceptionV3**, **ResNet50**, and **VGG16** — the app provides accurate identification of various blood cell types, including basophils, eosinophils, erythroblasts, lymphocytes, monocytes, neutrophils, and platelets.
 
-st.markdown("""
-<div style="text-align: justify;">
-            
-**LeukoVision** is an advanced AI-powered platform designed to analyze and classify blood cell images with precision.  
-Leveraging state-of-the-art deep learning models — **InceptionV3**, **ResNet50**, and **VGG16** — the app provides accurate identification of various blood cell types, including basophils, eosinophils, erythroblasts, lymphocytes, monocytes, neutrophils, and platelets.
+    With **LeukoVision**, you can: 
+    - Upload blood smear images or select from a curated gallery.
+    - Predict the correct cell type with confidence scores for each prediction.
+    - Visualize **Grad-CAM heatmaps** to understand which regions of the image influenced the model’s decision.
+    - Compare the performance of different models for research and educational purposes.
 
-With **LeukoVision**, you can: 
-- Upload blood smear images or select from a curated gallery.
-- Predict the correct cell type with confidence scores for each prediction.
-- Visualize **Grad-CAM heatmaps** to understand which regions of the image influenced the model’s decision.
-- Compare the performance of different models for research and educational purposes.
-
-This interactive tool is perfect for **researchers, educators, and medical professionals** looking to combine computational pathology with intuitive visualization.
-</div>
-""", unsafe_allow_html=True)
+    This interactive tool is perfect for **researchers, educators, and medical professionals** looking to combine computational pathology with intuitive visualization.
+    </div>
+    """, unsafe_allow_html=True)
