d3d3ad473d8f511531ecda395b6f81250834ddc1
ahmedali42 <ahmedali.4290@gmail.com>
2025-09-16 09:59:04 +0200

    update

--- Streamlit/pages/models.py
+++ Streamlit/pages/models.py
@@ -106,69 +106,69 @@ Grad-CAM visualization shows that the model consistently focuses on the cell its
 
 elif section == "ResNet50":
     st.subheader("ResNet50 🧬")
-    st.markdown("""
-        ### Overview  
-        <div style="text-align: justify;">
-        ResNet50 (Residual Network, 50 layers) is a deep convolutional neural network introduced by Microsoft Research in 2015.  
+#     st.markdown("""
+#         ### Overview  
+#         <div style="text-align: justify;">
+#         ResNet50 (Residual Network, 50 layers) is a deep convolutional neural network introduced by Microsoft Research in 2015.  
          
-        The key innovation in ResNet50 is the **residual block**, which introduces **skip connections** (or shortcuts) that allow the model to "skip" one or more layers. These shortcuts enable the network to directly pass information forward, making it easier to train very deep architectures.  
+#         The key innovation in ResNet50 is the **residual block**, which introduces **skip connections** (or shortcuts) that allow the model to "skip" one or more layers. These shortcuts enable the network to directly pass information forward, making it easier to train very deep architectures.  
  
-        ResNet50 became one of the most influential models in deep learning, forming the backbone of many modern architectures. Its ability to combine **depth with stability** makes it highly effective for tasks like blood cell classification.
-        </div>
-        """,unsafe_allow_html=True)
-    st.image(white_bg('./Streamlit/pages/images/resnet50/resnet50.png'), caption='Architecture diagram of ResNet50',use_container_width=True)
-    model = ResNet50(weights='imagenet')
-    with st.expander("See Full Model Summary"):
-        stream = io.StringIO()
-        model.summary(print_fn=lambda x: stream.write(x + "\n"))
-        st.code(stream.getvalue())
-    st.markdown('### Performace')
-    st.markdown(""" <div style="text-align: justify;">
-    The training accuracy approaches 99.85%, while the validation accuracy reaches 
-    97.90%, demonstrating the model’s strong ability to accurately classify different cell types.
-                </div>
-                """,unsafe_allow_html=True)
-    if st.toggle("Show ResNet50 loss and accuracy plot"):
-        st.image(white_bg('./Streamlit/pages/images/resnet50/loss_acc.png'), caption='Loss and accruacy plot from ResNet50 training',use_container_width=True)
-    st.markdown(""" <div style="text-align: justify;">
-                The test set shows a very high accuracy of 97.63% 
-                and that is reflected in the digonal form of the confusion matrix.
-                </div>
-                """,unsafe_allow_html=True)
-    if st.toggle("Show ResNet50 confusion matrix"):
-        st.image(white_bg('./Streamlit/pages/images/resnet50/cm.png'), caption='Confusion matrix of ResNet50 test set',use_container_width=True)
-    report = pd.read_csv("./Streamlit/pages/images/resnet50/class_report.txt", 
-                     sep="\s+", header=0,
-                     names=["Class", "Recall", "Specificity", "Precision", "F1-Score"])
-    report.index = report.index + 1
-    numeric_cols = report.select_dtypes(include="number").columns
-    styled = report.style.format({col: "{:.2f}" for col in numeric_cols}) \
-                        .set_properties(**{"text-align": "center"}) \
-                        .set_table_styles([{
-                            "selector": "th",
-                            "props": [("text-align", "center"), ("font-weight", "bold")]
-                        }])
-    st.markdown(""" <div style="text-align: justify;">
-                The classification metrics consistently range between 0.96 and 1.00, 
-                demonstrating that the ResNet50 model performs exceptionally well in 
-                distinguishing among different WBC subtypes. This highlights both the robustness of 
-                the model and its suitability for automated cell classification tasks.
-                </div>
-                """,unsafe_allow_html=True)
-    if st.toggle("Show classification report"):
-        st.dataframe(styled)
-    st.markdown('### Interpretability by Grad-CAM')
-    st.markdown(""" <div style="text-align: justify;">
-Grad-CAM visualization shows that the model consistently focuses on the cell itself while ignoring the 
-                background, suggesting that the classification is based on cytomorphological features 
-                of the cells rather than irrelevant image artifacts.
-                </div>
-                """,unsafe_allow_html=True)
-    if st.toggle("Show InceptionV3 Grad-CAM results"):
-        st.image(white_bg('./Streamlit/pages/images/resnet50/prediction.png'), 
-                 caption='Four randomly selected test set images with their ground-truth '
-                 'and predicted labels. (Bottom) Corresponding Grad-CAM visualizations highlighting ' \
-                 'the image regions most influential in the model’s predictions.',use_container_width=True)
+#         ResNet50 became one of the most influential models in deep learning, forming the backbone of many modern architectures. Its ability to combine **depth with stability** makes it highly effective for tasks like blood cell classification.
+#         </div>
+#         """,unsafe_allow_html=True)
+#     st.image(white_bg('./Streamlit/pages/images/resnet50/resnet50.png'), caption='Architecture diagram of ResNet50',use_container_width=True)
+#     model = ResNet50(weights='imagenet')
+#     with st.expander("See Full Model Summary"):
+#         stream = io.StringIO()
+#         model.summary(print_fn=lambda x: stream.write(x + "\n"))
+#         st.code(stream.getvalue())
+#     st.markdown('### Performace')
+#     st.markdown(""" <div style="text-align: justify;">
+#     The training accuracy approaches 99.85%, while the validation accuracy reaches 
+#     97.90%, demonstrating the model’s strong ability to accurately classify different cell types.
+#                 </div>
+#                 """,unsafe_allow_html=True)
+#     if st.toggle("Show ResNet50 loss and accuracy plot"):
+#         st.image(white_bg('./Streamlit/pages/images/resnet50/loss_acc.png'), caption='Loss and accruacy plot from ResNet50 training',use_container_width=True)
+#     st.markdown(""" <div style="text-align: justify;">
+#                 The test set shows a very high accuracy of 97.63% 
+#                 and that is reflected in the digonal form of the confusion matrix.
+#                 </div>
+#                 """,unsafe_allow_html=True)
+#     if st.toggle("Show ResNet50 confusion matrix"):
+#         st.image(white_bg('./Streamlit/pages/images/resnet50/cm.png'), caption='Confusion matrix of ResNet50 test set',use_container_width=True)
+#     report = pd.read_csv("./Streamlit/pages/images/resnet50/class_report.txt", 
+#                      sep="\s+", header=0,
+#                      names=["Class", "Recall", "Specificity", "Precision", "F1-Score"])
+#     report.index = report.index + 1
+#     numeric_cols = report.select_dtypes(include="number").columns
+#     styled = report.style.format({col: "{:.2f}" for col in numeric_cols}) \
+#                         .set_properties(**{"text-align": "center"}) \
+#                         .set_table_styles([{
+#                             "selector": "th",
+#                             "props": [("text-align", "center"), ("font-weight", "bold")]
+#                         }])
+#     st.markdown(""" <div style="text-align: justify;">
+#                 The classification metrics consistently range between 0.96 and 1.00, 
+#                 demonstrating that the ResNet50 model performs exceptionally well in 
+#                 distinguishing among different WBC subtypes. This highlights both the robustness of 
+#                 the model and its suitability for automated cell classification tasks.
+#                 </div>
+#                 """,unsafe_allow_html=True)
+#     if st.toggle("Show classification report"):
+#         st.dataframe(styled)
+#     st.markdown('### Interpretability by Grad-CAM')
+#     st.markdown(""" <div style="text-align: justify;">
+# Grad-CAM visualization shows that the model consistently focuses on the cell itself while ignoring the 
+#                 background, suggesting that the classification is based on cytomorphological features 
+#                 of the cells rather than irrelevant image artifacts.
+#                 </div>
+#                 """,unsafe_allow_html=True)
+#     if st.toggle("Show InceptionV3 Grad-CAM results"):
+#         st.image(white_bg('./Streamlit/pages/images/resnet50/prediction.png'), 
+#                  caption='Four randomly selected test set images with their ground-truth '
+#                  'and predicted labels. (Bottom) Corresponding Grad-CAM visualizations highlighting ' \
+#                  'the image regions most influential in the model’s predictions.',use_container_width=True)
 
 
 #It was designed to address the **vanishing gradient problem** that arises when training very deep networks. 
